9195b93284773fa46dfc99bfcc7b399416eb11c6
Safewaan <safwaanchowdhury26@gmail.com>
2025-11-15 22:15:36 -0500

    feat: add Login page with routing

---
+++ opulus-frontend/src/routes/login.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Login } from '@/pages/Login/Login';
+
+export const Route = createFileRoute('/login')({
+  component: Login,
+});
+
