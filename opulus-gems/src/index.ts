8f78890cf544ee8d2ffc46268ca7596666f2e956
Safewaan <safwaanchowdhury26@gmail.com>
2025-11-29 15:52:08 -0500

    cleanup

--- opulus-gems/src/index.ts
+++ opulus-gems/src/index.ts
@@ -73,33 +73,6 @@ export {
   SheetDescription,
 } from './components/ui/sheet';
 
-// Sidebar components
-export {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupAction,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarInput,
-  SidebarInset,
-  SidebarMenu,
-  SidebarMenuAction,
-  SidebarMenuBadge,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSkeleton,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-  SidebarProvider,
-  SidebarRail,
-  SidebarSeparator,
-  SidebarTrigger,
-  useSidebar,
-} from './components/ui/sidebar';
 
 // Skeleton component
 export { Skeleton } from './components/ui/skeleton';
