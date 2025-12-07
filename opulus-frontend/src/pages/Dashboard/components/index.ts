1ddf93155fc691cce24a39d7a780723745f377dc
Safewaan <safwaanchowdhury26@gmail.com>
2025-12-06 23:03:05 -0500

    app layout

--- opulus-frontend/src/pages/Dashboard/components/index.ts
+++ opulus-frontend/src/pages/Dashboard/components/index.ts
@@ -1,4 +1,3 @@
 export { ChartAreaInteractive } from "./ChartAreaInteractive";
 export { DataTable } from "./DataTable";
 export { SectionCards } from "./SectionCards";
-export { SiteHeader } from "./SiteHeader";
